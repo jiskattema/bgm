4577aa407f07ee8082806379609c3a07e572c8bd
Jisk Attema <jiskattema@gmail.com>
2025-03-22 15:14:58 +0100

    format printing

--- filter.go
+++ filter.go
@@ -39,7 +39,7 @@ func (f *Filter) Draw(ctx vxfw.DrawContext) (vxfw.Surface, error) {
 	var count_text string
 
 	if len(f.matches) == 1 {
-		count_text = f.matches[0]
+		count_text = fmt.Sprintf("%s [only]", f.matches[0])
 	} else {
 		if (f.cursor >= 0 && f.cursor < len(f.matches)) {
 			count_text = fmt.Sprintf("%s [%d of %d]", f.matches[f.cursor], f.cursor, len(f.matches))
